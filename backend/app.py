fe7e04b0963c1d89b010b29cffc37de73f4e6236
Jonathan Song <jonats10@uci.edu>
2024-04-18 13:37:00 -0700

    connecti0n fail error wip

--- backend/app.py
+++ backend/app.py
@@ -9,15 +9,12 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-# Dictionary to store active rooms
 rooms = {}
 
-# Serve the index.html file for the root URL
 @app.route('/')
 def serve_react():
     return send_from_directory(os.path.join(os.path.dirname(__file__), '..', 'my-react-app', 'build'), 'index.html')
 
-# Serve static files (JS, CSS, etc.) from subdirectories
 @app.route('/static/js/<path:path>')
 def serve_js(path):
     return send_from_directory(os.path.join(os.path.dirname(__file__), '..', 'my-react-app', 'build', 'static', 'js'), path)
@@ -26,12 +23,10 @@ def serve_js(path):
 def serve_css(path):
     return send_from_directory(os.path.join(os.path.dirname(__file__), '..', 'my-react-app', 'build', 'static', 'css'), path)
 
-# Serve image files from the static directory
 @app.route('/static/media/<path:path>')
 def serve_static(path):
     return send_from_directory(os.path.join(os.path.dirname(__file__), '..', 'my-react-app', 'build', 'static',  'media'), path)
 
-# Serve manifest.json
 @app.route('/manifest.json')
 def serve_manifest():
     return send_from_directory(os.path.join(os.path.dirname(__file__), '..', 'my-react-app', 'build'), 'manifest.json')
@@ -39,7 +34,6 @@ def serve_manifest():
 def generate_room_id():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
-# SocketIO event to host a room
 @socketio.on('host_room')
 def host_room():
     room_id = generate_room_id()
@@ -47,7 +41,6 @@ def host_room():
     print(rooms)
     emit('room_created', {'room_id': room_id})
 
-# SocketIO event to join a room
 @socketio.on('join_room')
 def join_room(data):
     room_id = data['room_id']
