347eff8b913b386d94cedb0f901ebc4f82b1f40d
Jonathan Song <jonats10@uci.edu>
2024-04-22 13:59:45 -0700

    add get request

--- backend/app.py
+++ backend/app.py
@@ -66,3 +66,31 @@ def join_room(data):
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
+
+# @app.route("/", methods=["POST", "GET"])
+# def home():
+#     session.clear()
+#     if request.method == "POST":
+#         name = request.form.get("name")
+#         code = request.form.get("code")
+#         join = request.form.get("join", False)
+#         create = request.form.get("create", False)
+
+#         if not name:
+#             return render_template("home.html", error="Please enter a name.", code=code, name=name)
+
+#         if join != False and not code:
+#             return render_template("home.html", error="Please enter a room code.", code=code, name=name)
+        
+#         room = code
+#         if create != False:
+#             room = generate_unique_code(4)
+#             rooms[room] = {"members": 0, "messages": []}
+#         elif code not in rooms:
+#             return render_template("home.html", error="Room does not exist.", code=code, name=name)
+        
+#         session["room"] = room
+#         session["name"] = name
+#         return redirect(url_for("room"))
+
+#     return render_template("home.html")
